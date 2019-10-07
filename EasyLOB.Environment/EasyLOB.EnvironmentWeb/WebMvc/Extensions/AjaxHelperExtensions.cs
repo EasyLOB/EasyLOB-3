073b6eb6dc07c30d802b66761aa7120d3f3bff9c
Siegmar Gieseler <siegmar.gieseler@siegmar.com.br>
2019-10-07 20:53:22 -0300

    -

--- EasyLOB.Environment/EasyLOB.EnvironmentWeb/WebMvc/Extensions/AjaxHelperExtensions.cs
+++ EasyLOB.Environment/EasyLOB.EnvironmentWeb/WebMvc/Extensions/AjaxHelperExtensions.cs
@@ -4,6 +4,22 @@ using System.Web.Mvc;
 // AjaxExtensions.ActionLink Method
 // https://msdn.microsoft.com/en-us/library/system.web.mvc.ajax.ajaxextensions.actionlink%28v=vs.118%29.aspxv
 
+// +------------------------+-----------------------------+
+// |      AjaxOptions       |       HTML attribute        |
+// +------------------------+-----------------------------+
+// | Confirm                | data-ajax-confirm           |
+// | HttpMethod             | data-ajax-method            |
+// | InsertionMode          | data-ajax-mode              |
+// | LoadingElementDuration | data-ajax-loading-duration  |
+// | LoadingElementId       | data-ajax-loading           |
+// | OnBegin                | data-ajax-begin             |
+// | OnComplete             | data-ajax-complete          |
+// | OnFailure              | data-ajax-failure           |
+// | OnSuccess              | data-ajax-success           |
+// | UpdateTargetId         | data-ajax-update            |
+// | Url                    | data-ajax-url               |
+// +------------------------+-----------------------------+
+
 // Chrome alt+title BUG
 
 namespace EasyLOB.Environment
@@ -18,7 +34,7 @@ namespace EasyLOB.Environment
             string imageTitle)
         {
             //string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" href=\"{2}\"><img alt=\"{4}\" class=\"{3}\" title=\"{4}\" /></a>",
-            string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" href=\"{2}\"><img class=\"{3}\" title=\"{4}\" /></a>",
+            string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" data-ajax-complete=\"zAjaxLoadComplete\" href=\"{2}\"><img class=\"{3}\" title=\"{4}\" /></a>",
                 id,
                 updateTargetId,
                 uri,
@@ -27,23 +43,25 @@ namespace EasyLOB.Environment
 
             return new MvcHtmlString(tag);
         }
-
-        public static string ImageLink(this AjaxHelper ajaxHelper,
+        /*
+        public static MvcHtmlString ZImageLink(this AjaxHelper ajaxHelper,
             string id,
             string uri,
             string updateTargetId,
             string imageClass,
-            string imageTitle)
+            string imageTitle,
+            string onComplete)
         {
-            //string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" href=\"{2}\"><img alt=\"{4}\" class=\"{3}\" title=\"{4}\" /></a>",
-            string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" href=\"{2}\"><img class=\"{3}\" title=\"{4}\" /></a>",
+            string tag = String.Format("<a id=\"{0}\" data-ajax=\"true\" data-ajax-mode=\"replace\" data-ajax-update=\"#{1}\" data-ajax-complete=\"{5}\" href=\"{2}\"><img class=\"{3}\" title=\"{4}\" /></a>",
                 id,
                 updateTargetId,
                 uri,
                 imageClass,
-                imageTitle);
+                imageTitle,
+                onComplete);
 
-            return tag;
+            return new MvcHtmlString(tag);
         }
+        */
     }
 }
